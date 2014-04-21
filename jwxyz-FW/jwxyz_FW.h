6392c246a8dee149c00f0ea116e9b8d93d6f5007
C.W. Betts <computers57@hotmail.com>
2014-04-20 22:56:16 -0600

    Begin work on OS X improvements. Create a framework for the OS X screen savers: this should cut down on size.

---
+++ jwxyz-FW/jwxyz_FW.h
@@ -0,0 +1,13 @@
+//
+//  jwxyz_FW.h
+//  jwxyz-FW
+//
+//  Created by C.W. Betts on 4/20/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jwxyz_FW : NSObject
+
+@end
---
+++ jwxyz-FW/jwxyz_FW.m
@@ -0,0 +1,13 @@
+//
+//  jwxyz_FW.m
+//  jwxyz-FW
+//
+//  Created by C.W. Betts on 4/20/14.
+//
+//
+
+#import "jwxyz_FW.h"
+
+@implementation jwxyz_FW
+
+@end
